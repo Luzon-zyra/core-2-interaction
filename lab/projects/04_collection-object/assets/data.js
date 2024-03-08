bca81c4cc92c31977032f318af62daea96eb209c
Luzon-zyra <146144186+Luzon-zyra@users.noreply.github.com>
2024-03-07 21:13:04 -0500

    update

---
+++ lab/projects/04_collection-object/assets/data.js
@@ -0,0 +1,128 @@
+// An array of objects representing my collection:
+var data = [
+    {
+       brand: 'Tom Ford',
+       perfume: 'Oud Wood',
+       type:'EDP',
+    },
+    {
+        brand: 'L Artisan',
+        perfume: 'Passage d Enfer',
+        type:'EDT',
+     },
+     {
+        brand: 'L Artisan',
+        perfume: 'Histoire d Orangers',
+        type:'EDP',
+     },
+     {
+        brand: 'Parle Moi de Parfum',
+        perfume: 'Mile High',
+        type:'EDP',
+     },
+     {
+        brand: 'Fueguia 1833',
+        perfume: 'Muskara Pelargonium',
+        type:'EDP',
+     },
+     {
+        brand: 'Fueguia 1833',
+        perfume: 'Rosa de la Patagonta',
+        type:'EDP',
+     },
+     {
+        brand: 'Parfum d Empire',
+        perfume: 'Acqua di scandola',
+        type:'EDP',
+     },
+     {
+        brand: 'Parfum d Empire',
+        perfume: 'Salute!',
+        type:'EDP',
+     },
+     {
+        brand: 'Parfum d Empire',
+        perfume: 'Eau Suave',
+        type:'EDP',
+     },
+     {
+        brand: 'Serge Lutens',
+        perfume: 'Fille en Aiguilles',
+        type:'EDP',
+     },
+     {
+        brand: 'Le labo',
+        perfume: 'Rose 31',
+        type:'EDP',
+     },
+     {
+        brand: 'Le labo',
+        perfume: 'The Matcha 26',
+        type:'EDP',
+     },
+     {
+        brand: 'Le labo',
+        perfume: 'The Noir 29',
+        type:'EDP',
+     },
+     {
+        brand: 'Le labo',
+        perfume: 'Ylang 49',
+        type:'EDP',
+     },
+     {
+        brand: 'Le labo',
+        perfume: 'Another 13',
+        type:'EDP',
+     },
+     {
+        brand: 'Le labo',
+        perfume: 'Oud 27',
+        type:'EDP',
+     },
+     {
+        brand: 'Le labo',
+        perfume: 'Jasmin 17',
+        type:'EDP',
+     },
+     {
+        brand: 'Le labo',
+        perfume: 'Santal 33',
+        type:'EDP',
+     },
+     {
+        brand: '19-69',
+        perfume: 'Capri',
+        type:'EDP',
+     },
+     {
+        brand: 'Diptyque',
+        perfume: 'Do Son',
+        type:'EDP',
+     },
+     {
+        brand: 'Diptyque',
+        perfume: 'Orpheon',
+        type:'EDP',
+     },
+     {
+        brand: 'Diptyque',
+        perfume: 'Tempo',
+        type:'EDP',
+     },
+     {
+        brand: 'Diptyque',
+        perfume: 'L Eau Papier',
+        type:'EDT',
+     },
+     {
+        brand: 'Diptyque',
+        perfume: 'Eau Moheli',
+        type:'EDT',
+     },
+     {
+        brand: 'Diptyque',
+        perfume: 'Philosykos',
+        type:'EDT',
+     },
+];
