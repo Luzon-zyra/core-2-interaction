c8fbf06ca6342d7ea30682600b43d832d745c4fb
Luzon-zyra <146144186+Luzon-zyra@users.noreply.github.com>
2024-03-19 14:37:48 -0400

    update

---
+++ studio/projects/02_elastic-collection/assets/js/script.js
@@ -0,0 +1,33 @@
+  // Randomly generate image sources and positions
+  const images = [
+    'page 2/assets/img/acqua di scandola.png',
+    'page 2/assets/img/apple brandy.png',
+    'page 2/assets/img/blue gin.png',
+    'page 2/assets/img/capri.png',
+    'page 2/assets/img/doson.png',
+    'page 2/assets/img/eau du memo.png',
+    'page 2/assets/img/eau rose.png',
+    'page 2/assets/img/eau suave.png',
+    'page 2/assets/img/fille en aiguilles.png',
+    'page 2/assets/img/for your love.png',
+    'page 2/assets/img/histoire d orangers.png',
+    'page 2/assets/img/jasmin 17.png',
+    'page 2/assets/img/mile high.png',
+    'page 2/assets/img/muskara pelargonium.png',
+    'page 2/assets/img/oud jaune intense.png',
+    'page 2/assets/img/oud wood.png',
+    // Add more image URLs as needed
+  ];
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    const container = document.body;
+    
+    images.forEach(imageSrc => {
+      const img = document.createElement('img');
+      img.src = imageSrc;
+      img.classList.add('random-image');
+      img.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
+      img.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
+      container.appendChild(img);
+    });
+  });
