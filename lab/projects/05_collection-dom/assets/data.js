d49769ec191c054b77368f96125a5d0e50ab29c4
Luzon-zyra <146144186+Luzon-zyra@users.noreply.github.com>
2024-05-05 03:04:07 -0400

    update

--- lab/projects/05_collection-dom/assets/data.js
+++ lab/projects/05_collection-dom/assets/data.js
@@ -10,174 +10,203 @@ var data = [
         name: 'Apple Brandy',
         type: 'EDP',
         note: 'Fruity',
-        color: 'orangered'
+        color: 'orangered',
+        image: 'apple brandy.png'
     },
     {
         name: 'Blue Gin',
         type: 'EDP',
         note: 'Critrus',
-        color: 'yellow'
+        color: 'yellow',
+        image: 'blue gin.png'
     },
     {
         name: 'Capri',
         type: 'EDP',
         note: 'Critrus',
-        color: 'yellow'
+        color: 'yellow',
+        image: 'capri.png'
     },
     {
         name: 'Doson',
         type: 'EDP',
         note: 'Floral',
-        color: 'pink'
+        color: 'pink',
+        image: 'doson.png'
     },
     {
         name: 'Eau du Memo',
         type: 'EDP',
         note: 'Critrus',
-        color: 'yellow'
+        color: 'yellow',
+        image: 'eau du memo.png'
     },
     {
         name: 'Eau Rose',
         type: 'EDT',
         note: 'Floral',
-        color: 'pink'
+        color: 'pink',
+        image: 'eau rose.png'
     },
     {
         name: 'Eau Suave',
         type: 'EDP',
         note: 'Floral',
-        color: 'pink'
+        color: 'pink',
+        image: 'eau suave.png'
     },
     {
         name: 'Fille en Aiguilles',
         type: 'EDP',
         note: 'Woody',
-        color: 'chocolate'
+        color: 'chocolate',
+        image: 'fille en aiguilles.png'
     },
     {
         name: 'For Your Love',
         type: 'EDP',
         note: 'Floral',
-        color: 'pink'
+        color: 'pink',
+        image: 'for your love.png'
     },
     {
         name: 'Histoire d Orangers',
         type: 'EDT',
         note: 'Critrus',
-        color: 'yellow'
+        color: 'yellow',
+        image: 'histoire d orangers.png'
     },
     {
         name: 'Jasmin 17',
         type: 'EDP',
         note: 'Floral',
-        color: 'pink'
+        color: 'pink',
+        image: 'jasmin 17.png'
     },
     {
         name: 'Mile High 38',
         type: 'EDP',
         note: 'Fruity',
-        color: 'orangered'
+        color: 'orangered',
+        image: 'mile high.png'
     },
     {
         name: 'Muskara Pelargonium',
         type: 'P',
         note: 'Green',
-        color: 'green'
+        color: 'green',
+        image: 'muskara pelargonium.png'
     },
     {
         name: 'Oud Jaune Intense',
         type: 'P',
         note: 'Woody',
-        color: 'chocolate'
+        color: 'chocolate',
+        image: 'oud jaune intense.png'
     },
     {
         name: 'Oud Wood',
         type: 'EDP',
         note: 'Woody',
-        color: 'chocolate'
+        color: 'chocolate',
+        image: 'oud wood.png'
     },
     {
         name: 'Passage d Enfer',
         type: 'EDP',
         note: 'Woody',
-        color: 'chocolate'
+        color: 'chocolate',
+        image: 'passage d enfer.png'
     },
     {
         name: 'Philosykos',
         type: 'EDT',
         note: 'Green',
-        color: 'green'
+        color: 'green',
+        image: 'philosykos edt.png'
     },
     {
         name: 'Philosykos',
         type: 'EDP',
         note: 'Woody',
-        color: 'chocolate'
+        color: 'chocolate',
+        image: 'philosykos.png'
     },
     {
         name: 'Promise',
         type: 'EDP',
         note: 'Woody',
-        color: 'chocolate'
+        color: 'chocolate',
+        image: 'promise.png'
     },
     {
         name: 'Rose 31',
         type: 'EDP',
         note: 'Floral',
-        color: 'pink'
+        color: 'pink',
+        image: 'rose 31.png'
     },
     {
         name: "Rose of No Man's Land",
         type: 'EDP',
         note: 'Floral',
-        color: 'pink'
+        color: 'pink',
+        image: "rose of no man's land.png"
     },
     {
         name: 'Salute!',
         type: 'EDP',
         note: 'Critrus',
-        color: 'yellow'
+        color: 'yellow',
+        image: 'salute!.png'
     },
     {
         name: 'Santal 33',
         type: 'EDP',
         note: 'Woody',
-        color: 'chocolate'
+        color: 'chocolate',
+        image: 'santal 33.png'
     },
     {
         name: 'Secretions Magnifiques',
         type: 'EDP',
         note: 'Marine',
-        color: 'slateblue'
+        color: 'slateblue',
+        image: 'secretions magnifiques.png'
     },
     {
         name: 'Sliver Mountian Water',
         type: 'EDP',
         note: 'Musky',
-        color: 'aliceblue'
+        color: 'aliceblue',
+        image: 'sliver mountian water.png'
     },
     {
         name: 'Super Cedar',
         type: 'EDP',
         note: 'Woody',
-        color: 'chocolate'
+        color: 'chocolate',
+        image: 'super cedar.png'
     },
     {
         name: 'The Matcha 26',
         type: 'EDP',
         note: 'Woody',
-        color: 'chocolate'
+        color: 'chocolate',
+        image: 'the matcha 26.png'
     },
     {
         name: 'White Neroli',
         type: 'EDP',
         note: 'Musky',
-        color: 'aliceblue'
+        color: 'aliceblue',
+        image: 'white neroli.png'
     },
     {
         name: 'You Or Someone Like You',
         type: 'EDP',
         note: 'Green',
-        color: 'green'
+        color: 'green',
+        image: 'you or someone like you.png'
     },
 ];
